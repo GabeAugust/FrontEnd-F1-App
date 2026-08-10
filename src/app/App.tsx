169bac3dea3ffeccc4529e8b9c78db56fc37d2c0
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-10 20:49:58 -0300

    feat:header and background image

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,9 +3,40 @@ const App = () => {
   return (
   
     <main className="min-h-screen bg-zinc-950 text-white">
-        <Header/>
+      <Header />
+
+      <section
+        className="
+      relative
+      min-h-screen
+      bg-[linear-gradient(90deg,rgba(0,0,0,0.95)_0%,rgba(0,0,0,0.70)_42%,rgba(0,0,0,0.30)_75%,rgba(0,0,0,0.55)_100%),url('../src/assets/f1-bg.jpg')]
+      bg-cover
+      bg-center
+      bg-no-repeat
+    "
+      >
+        {/* seu conteúdo */}
+
+        <div className="absolute inset-0 bg-linear-to-r from-black via-black/50 to-transparent" />
+
+        <div
+          className="
+        pointer-events-none
+        absolute
+        inset-x-0
+        bottom-0
+        h-[45vh]
+        bg-linear-to-b
+        from-transparent
+        via-zinc-950/70
+        to-zinc-950
+      "
+        />
+      </section>
+
+      <section className="min-h-screen bg-zinc-950">
+      </section>
     </main>
-    
   );
 }
 
