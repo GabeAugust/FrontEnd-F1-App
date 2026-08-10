530b2a84d6a43d63faf8c1a622641f1d6432c2f5
Gabriel Augusto Gonçalves Pereira <gabriel.pereira@trocafone.com>
2026-08-10 14:43:24 -0300

    feat: logo f1 and header structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,11 @@
+import Header from "../components/Header.tsx"
 const App = () => {
   return (
-    <main className="min-h-screen bg-zinc-950 p-8 text-white">
-      <h1 className="text-4xl font-bold text-red-600">
-        Formula 1
-      </h1>
-
-      <p className="mt-3 text-zinc-400">
-        Explore drivers, teams and race results.
-      </p>
-
-      <button className="mt-6 rounded-lg bg-red-600 px-5 py-3 font-semibold transition hover:bg-red-700">
-        View drivers
-      </button>
+  
+    <main className="min-h-screen bg-zinc-950 text-white">
+        <Header/>
     </main>
+    
   );
 }
 
