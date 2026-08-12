c136f5bbb3faf404cc81efbe34cc096ecde3c272
Gabriel Augusto Gonçalves Pereira <gabriel.pereira@trocafone.com>
2026-08-12 13:40:34 -0300

    feat: add dark theme and light theme and fix header component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,40 +1,25 @@
 import Header from "../components/Header.tsx"
-const App = () => {
-  return (
-  
-    <main className="min-h-screen bg-zinc-950 text-white">
-      <Header />
+import React, { useState } from 'react';
 
-      <section
-        className="
-      relative
-      min-h-screen
-      bg-[linear-gradient(90deg,rgba(0,0,0,0.95)_0%,rgba(0,0,0,0.70)_42%,rgba(0,0,0,0.30)_75%,rgba(0,0,0,0.55)_100%),url('../src/assets/f1-bg.jpg')]
-      bg-cover
-      bg-center
-      bg-no-repeat
-    "
-      >
+const App = () => {
+  const [isDark, setIsDark] = useState(true);
+
+  function toggleTheme() {
+    setIsDark((currentTheme) => !currentTheme);
+  }
+
+  return (
+
+    <main className={`app ${isDark ? "dark-theme" : "light-theme"}`}>
+      <Header isDark={isDark} onToggleTheme={toggleTheme} />
+
+      <section className="hero">
         {/* seu conteúdo */}
 
-        <div className="absolute inset-0 bg-linear-to-r from-black via-black/50 to-transparent" />
-
-        <div
-          className="
-        pointer-events-none
-        absolute
-        inset-x-0
-        bottom-0
-        h-[45vh]
-        bg-linear-to-b
-        from-transparent
-        via-zinc-950/70
-        to-zinc-950
-      "
-        />
+        <div className="hero-bottom-fade" />
       </section>
 
-      <section className="min-h-screen bg-zinc-950">
+      <section className="page-section">
       </section>
     </main>
   );
